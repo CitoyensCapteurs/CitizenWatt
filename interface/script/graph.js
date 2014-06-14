2167eb750400f4472d35d9ae0f4804d4efce3cda
Élie Michel <elie.michel@ens.fr>
2014-06-14 16:23:39 +0200

    Une petite coquille corrigée et les paramètres placés en début de graph.js

--- interface/script/graph.js
+++ interface/script/graph.js
@@ -1,4 +1,8 @@
 
+var URL = 'test.json'
+  , MAX_POWER = 2500
+  ;
+
 (function(){
 	
 	var graph = document.getElementById('graph_values')
@@ -6,7 +10,7 @@
 	  , border = 2 // #graph_values .rect
 	  ;
 	
-	function addRect(nth, height) {
+	function addRect(height) {
 		var div = document.createElement('div');
 		var blank = document.createElement('div');
 		var color = document.createElement('div');
@@ -26,24 +30,28 @@
 		blank.style.height = (100 - height) + '%';
 	}
 	
-	for (var i = 0 ; i < 50 ; i++) {
-		addRect(i, 40 * (Math.sin(i/1.) + 1.1));
+	for (var i = 0 ; i < 10 ; i++) {
+		addRect(40 * (Math.sin(i/1.) + 1.1));
 	}
 
 	var req = new XMLHttpRequest();
 	function update() {
-		req.open('GET', '[URL]', true);
+		req.open('GET', URL, true);
+		req.send();
 		req.onreadystatechange = function() {
 	        if (req.readyState == 4) {
+	        	console.log(req.responseText);
 	            var data = JSON.parse(req.responseText);
 	            for(var i = 0 ; i < data.length ; ++i) {
-	            	addRect(data[i].power)
+	            	addRect(data[i].power / MAX_POWER);
 	            }
 	        }
 	    }
 
-		setTimeout(update, 1000);
+		setTimeout(update, 10000);
 	}
+
+	update();
 
 	
 })();
