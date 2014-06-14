25f12d46c110350c646adf1762c34c2fa93f0e39
Élie Michel <elie.michel@ens.fr>
2014-06-14 15:42:33 +0200

    Graphe aligné à droite

--- interface/script/graph.js
+++ interface/script/graph.js
@@ -1,18 +1,29 @@
 
 (function(){
 	
-	var graph = document.getElementById('graph')
+	var graph = document.getElementById('graph_values')
 	  , size = 12
 	  , border = 2
 	  ;
 	
 	function addRect(nth, height) {
 		var div = document.createElement('div');
+		var blank = document.createElement('div');
+		var color = document.createElement('div');
 		graph.appendChild(div);
-		div.className = 'rect ' + (height > 33.3 ? (height >= 66.7 ? 'red-day' : 'orange-day') : 'yellow-day');
+		div.appendChild(blank);
+		div.appendChild(color);
+
+		div.className = 'rect';
 		div.style.width = size + 'px';
-		div.style.left = border + ((size + border) * nth) + 'px';
-		div.style.height = height + '%';
+
+		color.className = 'color ' + (height > 33.3 ? (height >= 66.7 ? 'red-day' : 'orange-day') : 'yellow-day');
+		color.style.width = size + 'px';
+		color.style.height = height + '%';
+
+		blank.className = 'blank';
+		blank.style.width = size + 'px';
+		blank.style.height = (100 - height) + '%';
 	}
 	
 	for (var i = 0 ; i < 50 ; i++) {
